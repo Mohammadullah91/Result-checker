981decf6b9c2d1df28e0fc1e08ffa03bf83e020f
Mohammad Ullah Siam <125947254+Mohammadullah91@users.noreply.github.com>
2024-02-14 10:54:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function checkResult() {
+    var roll = document.getElementById("roll").value;
+    var examYear = document.getElementById("examYear").value;
+
+    var apiUrl = `https://sresult.bise-ctg.gov.bd/s${examYear}/individual/result.php`;
+    var requestData = `roll=${roll}&button2=Submit`;
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: requestData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("resultContainer").innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
